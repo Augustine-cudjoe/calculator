c13017876e298bd03eda93d89a7baffcb0c10296
Augustine Cudjoe <augustinecudjoe@Augustines-MacBook-Pro.local>
2024-02-01 09:59:58 +0100

    calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import { Row,Col } from 'react-bootstrap';
+
 
 function App() {
+  const [value,setValue]=useState(" ")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Calculator</h1>
+       <div className='mb-4 mt-4 topInput' >
+       <input  size='lg' type='text'  value={value} />
+       </div>
+       <Row className='row mx-auto py-3 '>
+      
+         <Col md="12" className=' first-row'>
+         <input type='button'  value="AC"  className='reset' onClick={(e)=>setValue(" ")}/>
+         <input type='button'  value="DE" className='delete'  onClick={(e)=>setValue(value.slice(0,-1))}/>
+         <input type='button'  value="." onClick={(e)=>setValue(value + e.target.value)}/>
+         <input type='button'  value="/" className='right-corner' onClick={(e)=>setValue(value +  e.target.value)}/>
+         </Col>
+
+         <Col md="12">
+         <input type='button'  value="7" onClick={(e)=>setValue(value +  e.target.value)}/>
+         <input type='button'  value="8" onClick={(e)=>setValue(value +  e.target.value)}/>
+         <input type='button'  value="9" onClick={(e)=>setValue(value +  e.target.value)}/>
+         <input type='button'  value="*" className='right-corner' onClick={(e)=>setValue(value +  e.target.value)}/>
+         </Col>
+
+         <Col md="12">
+         <input type='button'  value="4" onClick={(e)=>setValue(value +  e.target.value)}/>
+         <input type='button'  value="5" onClick={(e)=>setValue(value +  e.target.value)}/>
+         <input type='button'  value="6" onClick={(e)=>setValue(value +  e.target.value)}/>
+         <input type='button'  value="-" className='right-corner' onClick={(e)=>setValue(value +  e.target.value)}/>
+         </Col>
+
+         <Col md="12">
+         <input type='button'  value="1" onClick={(e)=>setValue(value +  e.target.value)}/>
+         <input type='button'  value="2" onClick={(e)=>setValue(value +  e.target.value)}/>
+         <input type='button'  value="3" onClick={(e)=>setValue(value +  e.target.value)}/>
+         <input type='button'  value="+" className='right-corner'onClick={(e)=>setValue(value +  e.target.value)}/>
+         </Col>
+         <Col md="12" className='down'>
+         <input type='button'  value="00" className='zero' onClick={(e)=>setValue(value +  e.target.value)}/>
+         <input type='button'  value="0" onClick={(e)=>setValue(value +  e.target.value)}/>
+         <input type='button'  value="=" className='right-corner' onClick={(e)=>setValue(eval(value))}/>
+    
+         </Col>
+        </Row>
     </div>
   );
 }
